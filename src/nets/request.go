9a252b475b42fdc7b5869be72a85f1bcbfa57a5d
Ellery Zhang <allotory@msn.com>
2019-08-18 20:00:41 +0800

    feat: packaging request.

---
+++ src/nets/request.go
@@ -0,0 +1,21 @@
+package nets
+
+import "topaza/interfaces"
+
+type Request struct {
+	// 已经和客户端建立好的连接
+	conn interfaces.IConnection
+
+	// 客户端请求数据
+	data []byte
+}
+
+// 获取当前连接
+func(r *Request) Connection() interfaces.IConnection {
+	return r.conn
+}
+
+// 获取请求的消息数据
+func(r *Request) Data() []byte {
+	return r.data
+}
